3504fe4f707f916a8fb37a2c4ce6a8f58b73051b
YoonJunHyeon <qmdldos@gmail.com>
2023-10-17 09:12:54 +0900

    마지막 저장

--- app.js
+++ app.js
@@ -10,24 +10,64 @@ console.log('hi');
 
 // 모르는 것 : promise, XMLHttpRequest
 
-// ! XMLHttpRequest 예시
-function reqListener(){
-  console.log(this.responseText);
-}
-
-const req = new XMLHttpRequest();
-// req.addEventListener('load', reqListener);
-// req.open("POST","http://www.example.org/example.txt")
-// req.send();
-
-req.open('post','http://jsonplaceholder.typicode.com/todos/1');
-req.setRequestHeader("Custom-Header1", "hello");
-req.setRequestHeader("Custom-Header2", "test");
-
 
 // * get 방식과 post 방식은 작성법이 다르다. 내장함수
 // *매개변수 작성할 것들
 // method - post, body - post payload, header - 추가 http 요청 헤더
 // * fetch()메서드를 활용
 // url에 post 요청을 보냄 / 
-// fetch()
+
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method : "POST",
+  headers : {
+    'Accept' : 'application/json',
+    "Content-type" : 'application/json'
+  },
+  body : JSON.stringify({"id" : 78912})
+})
+.then(response => response.json())
+.then(response => console.log(JSON.stringify(response)))
+// {"ID" : 101}
+  
+// ! XMLHttpRequest 예시
+// const data = JSON.stringify({
+//   title: 'foo',
+//   body: 'bar',
+//   userId: 1
+// });
+
+// const xhr = new XMLHttpRequest();
+
+// xhr.onload = () => {
+//   if (xhr.status === 201) {
+//     const post = JSON.parse(xhr.responseText);
+//     console.log(post);
+//   } 
+// };
+
+// xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts', true);
+// xhr.setRequestHeader('Content-Type', 'application/json');
+// xhr.send(data);
+//{title: 'foo', body: 'bar', userId: 1, id: 101}
+
+// ! XMLHttpRequest 예시 2
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    console.log(xhr.status); // 201
+    console.log(xhr.responseText); // DATA 그대로..
+  }};
+
+let data = `{
+  "Id": 78912,
+  "Customer": "Jason Sweet",
+  "Quantity": 1,
+  "Price": 18.00
+}`;
+
+xhr.send(data);
